7054e767d64be9d7df65d74857d248288907788a
maxjunior98 <69329401+maxjunior98@users.noreply.github.com>
2021-04-13 16:01:25 -0300

    Reescrito em funções

--- codigoVideo.py
+++ codigoVideo.py
@@ -22,141 +22,161 @@ def size_y_matrix(matrix):           #funcao para pegar altura do video
     rows = len(matrix)
     return int(rows)
 
-def get_fps(pathVideo):
+def get_fps(pathVideo):              #funcao para pegar fps do video
     cam = cv.VideoCapture(pathVideo)
     fps = cam.get(cv.CAP_PROP_FPS)
     return fps
 
-def convert_to_video(pathOut, fps, frames_array, height, width, len_video):
-    size = (width, height)
-    out = cv.VideoWriter(pathOut, cv.VideoWriter_fourcc(*'DIVX'), fps,size)
-    for k in range(len_video):
-        out.write(frames_array[k])
-    out.release()
+def get_matrix_imgs(path):
+    matrix = []
+    cap = cv.VideoCapture(path)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        try:
+            frame = frame [300:700,700:1200,:]              #PEGANDO PARTE DO VIDEO
+            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)    #CONVERTE PARA GRAY
+            matrix.append(gray)
+        except:
+            break
+    cap.release()
+    return matrix
+
+def get_frame(matrix, lenpath):
+    matrix_exit = []
+    for i in range(lenpath):
+       matrix_exit.append(int(matrix[i][250][350]))
+    matrix_exit = np.array(matrix_exit) 
+    return matrix_exit
+
+def get_analise_frame(frame_pixel, lenpath):
+    normalizado = []
+    amplified = []
+    vet_var = []
+    for i in range(lenpath):
+        normalizado.append(frame_pixel[i])
+        if(i>0 and frame_pixel[i] != frame_pixel[i-1]):
+            vet_var.append([i, frame_pixel[i-1], frame_pixel[i+1]])
+    for i in range(len(vet_var) - 1):
+        casa = vet_var[i][0]
+        casaNext = vet_var[i+1][0]
+        if(casaNext - casa < 13):
+            prevalue = vet_var[i][1]
+            nextvalue = vet_var[i+1][2]
+            if(prevalue == nextvalue):
+                for x in range(casa, casaNext):
+                    normalizado[x] = prevalue
+    for i in range(lenpath):
+        if(i == 0):
+            amplified.append(normalizado[i])
+        else:
+            if(normalizado[i] == normalizado[i-1]):
+                amplified.append(amplified[i - 1])
+            else:
+                if(normalizado[x] > normalizado[i - 1]):
+                    delta = normalizado[i] - normalizado[i-1]
+                    delta = 5 * delta
+                    if((amplified[i-1] + delta)>255):
+                        amplified.append(255)
+                    else:
+                        amplified.append(amplified[i-1] + delta)
+                else:
+                    delta = normalizado[i-1] - normalizado[i]
+                    delta = 5 * delta
+                    if((amplified[i-1] + delta)<0):
+                        amplified.append(0)
+                    else:
+                        amplified.append(amplified[i-1] - delta)
+    tempo = np.linspace(0, lenpath, num=lenpath, endpoint=True, dtype=int)
+    plt.figure()
+    #plt.xlabel
+    #plt.ylabel
+    #plt.title
+    #plt.ylim
+    plt.subplot(311)
+    plt.plot(tempo, frame_pixel)
+    plt.subplot(312)
+    plt.plot(tempo, normalizado)
+    plt.subplot(313)
+    plt.plot(tempo, amplified)
+    
+def show_video(video):
+    for i in range(180, 500):
+        cv.imshow('figure 1' ,video[i])
+        k = cv.waitKey(30)
+        if(k == 33):
+            break
 
 
-path = 'max.avi'
+def show_results(Original, Analise1, Analise2):
+    for i in range(180, 500):
+        cv.imshow('figure 1' ,Original[i])
+        k = cv.waitKey(30)
+        if(k == 33):
+            break
+    for i in range(180, 500):
+        cv.imshow('figure 2' ,Analise1[i])
+        k = cv.waitKey(30)
+        if(k == 33):
+            break    
+    for i in range(180, 500):
+        cv.imshow('figure 3' ,Analise2[i])
+        k = cv.waitKey(30)
+        if(k == 33):
+            break
+
+def amplify_video(Original, lenpath, altura, largura):
+    Amplified = []
+    for index in range(lenpath):
+        Amplified.append(Original[index].copy())
+    
+    for index_height in range(altura):
+        for index_width in range(largura):
+            for index_frame in range(180, 500):
+                value = int(Original[index_frame][index_height][index_width])
+                prevalue = int(Original[index_frame - 1][index_height][index_width])
+                if(value != prevalue):
+                    dif = value - prevalue
+                    dif = 5*dif
+                    if(dif > 0):
+                        value =  value + dif
+                        if(value > 255):
+                            value = 255
+                        Amplified[index_frame][index_height][index_width] = value
+                    else:
+                        value = value + dif
+                        if(value < 0):
+                            value = 0
+                        Amplified[index_frame][index_height][index_width] = value
+                else:
+                    Amplified[index_frame][index_height][index_width] = Amplified[index_frame - 1][index_height][index_width]
+    return Amplified
+
+        
+    
+
+path = './BaseVideos/max.avi'
 lenpath = lenght_of_video(path)     #lenght do video
-fpsVideo = get_fps(path)            #frames per second do video
-pathExit = './results/test1.avi'        
-
-pixels = []         #LISTA DE MATRIZ COM OS VALORES DOS PIXELS DE CADA FRAME
-cap = cv.VideoCapture(path)
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    try:
-        frame = frame [300:700,700:1200,:]              #PEGANDO PARTE DO VIDEO
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)    #CONVERTE PARA GRAY
-        pixels.append(gray)
-    except:
-        break
-
+fpsVideo = get_fps(path)            #frames per second do video      
+pixels = get_matrix_imgs(path)              #LISTA DE MATRIZ DE CADA FRAME
 size_largura = size_x_matrix(pixels[0])     #pegar altura e largura do video
 size_altura = size_y_matrix(pixels[0])     
+p1 = get_frame(pixels, lenpath)
 
-p1 = []
-amplified = []
-tempo = np.linspace(0, lenpath, num=lenpath, endpoint=True, dtype=int)
+#result1 = amplify_video(pixels, lenpath, size_altura, size_largura)
 
-ANALISE GRÁFICA DO COMPORTAMENTO DE UM PIXEL PARA DEFINIR FATOR DE AMPLIACAO
-for i in range(len(tempo)):
-    p1.append(int(pixels[i][300][400]))
-p1 = np.array(p1)
 
-for x in range(len(tempo)):
-    if(x == 0):
-        amplified.append(p1[x])
-    else:
-        if(p1[x] == p1[x-1]):
-            amplified.append(amplified[x - 1])
-        else:
-            print('FRAME')
-            print(x)
-            if(p1[x] > p1[x - 1]):
-                print('fator')
-                delta = p1[x] - p1[x-1]
-                print(delta)
-                print('fator multiplicado')
-                delta = 5 * delta
-                print(delta)
-                if((amplified[x-1] + delta)>255):
-                    amplified.append(255)
-                else:
-                    amplified.append(amplified[x-1] + delta)
-            else:
-                print('fator')
-                delta = p1[x-1] - p1[x]
-                print(delta)
-                print('fator multiplicado')
-                delta = 5 * delta
-                print(delta)
-                if((amplified[x-1] - delta)<0):
-                    amplified.append(0)
-                else:
-                    amplified.append(amplified[x-1] - delta)
 
-plt.figure()
-plt.subplot(121)
-plt.plot(tempo, p1)
-plt.subplot(122)
-plt.plot(tempo, amplified)
 
-Matrix_amplified = []
-for index in range(lenpath):
-    Matrix_amplified.append(pixels[index].copy())
-
-for index_height in range(size_altura):
-    for index_width in range(size_largura):
-        for index_frame in range(180, 500):
-            value = int(pixels[index_frame][index_height][index_width])
-            prevalue = int(pixels[index_frame - 1][index_height][index_width])
-            # print('PIXEL:')
-            # print(index_width)
-            # print(index_height)
-            # print('FRAME')
-            # print(index_frame)
-            # print('Valor')
-            # print(value)
-            # print('Valor Anterior')
-            # print(prevalue)
-            if(value != prevalue):
-                # print('DIF')
-                dif = value - prevalue
-                # print(dif)
-                dif = 2*dif
-                # print('DIF AUMENTADA')
-                # print(dif)
-                if(dif > 0):
-                    value =  value + dif
-                    if(value > 255):
-                        value = 255
-                    Matrix_amplified[index_frame][index_height][index_width] = value
-                else:
-                    value = value + dif
-                    if(value < 0):
-                        value = 0
-                    Matrix_amplified[index_frame][index_height][index_width] = value
-            else:
-                Matrix_amplified[index_frame][index_height][index_width] = Matrix_amplified[index_frame - 1][index_height][index_width]
-         
-
-    
-input("Press Enter to continue...")
-
-for mmmm in range(180, 500):
-    cv.imshow('figure 2' ,Matrix_amplified[mmmm])
-    k = cv.waitKey(20)
-    if(k == 33):
-        break
-
-input("Press Enter to continue...")
-
-video_exit = pixels
-for mmmm in range(lenpath):
-    cv.imshow('figure 2' ,video_exit[mmmm])
-    k = cv.waitKey(20)
-    if(k == 33):
-        break
-
-cap.release()
 #convert_to_video(pathExit, fpsVideo, video_exit, size_altura, size_largura, lenpath)
+
+#encontrar formas de variaçoes pequenas serem ignoradas
+#janela de frames anteriores
+#verificar frames por segundo, talvez ignorar frames
+#fixar camera para gravação
+#variancia e desvio padrão
+#np.var variancia, np.std desvio padrao
+#destacar apenas onde variar (imagem nova com tudo 0)
+
+#detecção de anomalias
+
+
